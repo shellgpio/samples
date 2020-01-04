1ac6b9f070ee4eee0deca77781b5ca76464b9816
potatodiy <tudoudiangong@163.com>
2020-01-04 17:37:23 +0000

    up

--- boat_nanopi_neo/back.sh
+++ boat_nanopi_neo/back.sh
@@ -1,4 +1,4 @@
 ./shellgpio/digiwrite.sh 198 0
 ./shellgpio/digiwrite.sh 199 1
-./shellgpio/digiwrite.sh 200 0
-./shellgpio/digiwrite.sh 201 1
+./shellgpio/digiwrite.sh 200 1
+./shellgpio/digiwrite.sh 201 0
--- boat_nanopi_neo/forward.sh
+++ boat_nanopi_neo/forward.sh
@@ -1,4 +1,4 @@
 ./shellgpio/digiwrite.sh 198 1
 ./shellgpio/digiwrite.sh 199 0
-./shellgpio/digiwrite.sh 200 1
-./shellgpio/digiwrite.sh 201 0
+./shellgpio/digiwrite.sh 200 0
+./shellgpio/digiwrite.sh 201 1
